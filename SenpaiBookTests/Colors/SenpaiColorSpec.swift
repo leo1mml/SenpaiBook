07d7bdee517c6a0b26bb4fef1743777027c6d10f
Leonel Menezes <mml_leo@hotmail.com>
2019-06-25 23:25:24 -0300

    Adds testing dependencies

---
+++ SenpaiBookTests/Colors/SenpaiColorSpec.swift
@@ -0,0 +1,21 @@
+//
+//  SempaiColorSpec.swift
+//  SenpaiBookTests
+//
+//  Created by Leonel Menezes on 25/06/19.
+//  Copyright © 2019 Leonel Menezes. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable
+import SenpaiBook
+
+class SenpaiColorScpec: QuickSpec {
+    override func spec() {
+        it("has to return a color") {
+            expect(SenpaiColor.main).toNot(beNil())
+        }
+    }
+}
+
