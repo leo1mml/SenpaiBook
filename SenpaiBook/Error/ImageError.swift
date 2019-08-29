3fee4189079599c7cfa2e4f9adb5e0f6c144b5f4
Leonel <mml_leo@hotmail.com>
2019-08-29 16:31:14 -0300

    Fixes message from url error

--- SenpaiBook/Error/ImageError.swift
+++ SenpaiBook/Error/ImageError.swift
@@ -10,7 +10,7 @@ import Foundation
 
 enum ImageError: String, Error, CustomStringConvertible {
     
-    case invalidDataFormat = "Could not decode and image from the given data object."
+    case invalidDataFormat = "Could not decode an image from the given data object."
     
     var description: String {
         return self.rawValue
